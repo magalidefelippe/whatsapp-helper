831a25d821a24f796e4c061b3ece7c09dba1393c
Maga Defelippe <magalidefelippeph@gmail.com>
2021-06-13 20:24:43 -0300

    First commit, UI structure finish

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <container class="mt-4" />
+  </div>
+</template>
+
+<script>
+import Container from './components/container.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Container
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  background-color: #F5D8A9;
+}
+html{
+  background-color: #F5D8A9;
+}
+</style>
---
+++ src/components/container.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row align-h="center">
+      <b-card
+        no-body
+        border-variant="primary"
+        header="primary"
+        header-bg-variant="primary"
+        header-text-variant="white"
+        style="width: 80%"
+      >
+        <template #header>
+          <h4 class="mb-0">WhatsApp helper!</h4>
+        </template>
+
+        <b-card-body>
+          <b-card-sub-title class="mb-2">Code by Magascript</b-card-sub-title>
+          <b-card-text>
+            <b-input-group class="mt-3">
+              <vue-country-code
+                :preferredCountries="['ar', 'es', 'us', 'br', 've', 'uy']"
+              >
+              </vue-country-code>
+              <b-form-input></b-form-input>
+              <b-input-group-append>
+                <b-button variant="outline-primary">
+                  <b-icon icon="mic"> </b-icon>
+                </b-button>
+              </b-input-group-append>
+            </b-input-group>
+
+            <b-button class="mt-3" block variant="outline-primary">
+              <b-spinner v-show="loading" small type="grow"></b-spinner>
+              Llevame a whatsapp!</b-button
+            >
+          </b-card-text>
+        </b-card-body>
+      </b-card>
+    </b-row>
+  </b-container>
+</template>
