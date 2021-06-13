d41d64e81869c8fbadcd8da8b9f2d9e462e3fb3d
Maga Defelippe <magalidefelippeph@gmail.com>
2021-06-13 20:52:37 -0300

    Added test about check if the number is not empty

--- src/Model/Number.test.js
+++ src/Model/Number.test.js
@@ -3,5 +3,9 @@ import {Number} from './Number';
 describe("Number class use cases", () => {
     const number = new Number();
 
+    test("Validate if the number is not empty", () => {
+        const validation = number.checkEmpty();
+        expect(validation).toThrow(Error)
+    })
 
 }) 
