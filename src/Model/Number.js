82ad0feb9d50ed082b1e83715d16461d465db053
Maga Defelippe <magalidefelippeph@gmail.com>
2021-06-13 21:19:02 -0300

    Added checkCode function and concat function, now the test pass

--- src/Model/Number.js
+++ src/Model/Number.js
@@ -7,4 +7,17 @@ export class Number {
     checkPhone(){
         if(this.phone == null || this.phone < 10) throw new Error('Numero muy corto')
     }
+
+    concat(){
+        return this.code.concat(this.phone);
+    }
+
+    checkCode(number){
+        const length = this.code.length;
+        if(number.slice(0, length) === this.code){
+            return number;
+        }else {
+            return this.concat();
+        }
+    }
 }
